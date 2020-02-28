cec1327cd50745e9416729708c2d65b4acaa4cbd
ejiec004 <010ejiec004@gmail.com>
2020-02-28 15:35:40 +0530

    two exercises ex2.rb and ex3.rb

---
+++ ex1.rb
@@ -0,0 +1,4 @@
+puts "Hello world"
+puts " hi!"
+puts " say 'hi' "
+
---
+++ ex2.rb
@@ -0,0 +1,8 @@
+#A comment, this is so you can read your program later
+#Anything after the # us ignored by ruby
+
+puts "I could have code like this" # and the comment after is ignored
+
+#You can also use a comment
+
+puts "This will run."
